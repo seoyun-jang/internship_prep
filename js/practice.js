97196e5187ff63efeaff8da4b68b2c294fc76494
seoyun-jang <seoyun_22@naver.com>
2023-07-13 16:07:25 +0900

    ESG인턴십 2일차 js 연습문제

--- js/practice.js
+++ js/practice.js
@@ -79,3 +79,21 @@ for(i=1;i<31;i++){
     }
 } 
 
+//solution
+
+function fizzbuzz(){
+    for(i=1; i<31; i++){
+        if (i % 3 ==0 && i % 5 ==0){
+            console.log('fizzbuzz')
+        }else if (i % 3 == 0){
+            console.log('fizz')
+        } else if (i % 5 == 0){
+            console.log('buzz')
+        } else{
+            console.log(i)
+        }
+
+    }
+}
+
+fizzbuzz()
